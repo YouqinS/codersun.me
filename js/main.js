417376fc9a50be216a6e8d564da4c725a6a0a691
YouqinS <33859677+YouqinS@users.noreply.github.com>
2018-11-11 13:16:48 +0200

    header hidden on scroll down

--- js/main.js
+++ js/main.js
@@ -24,3 +24,74 @@ function validateForm() {
     return true;
   }
 }
+
+
+
+
+let prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+let currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("header").style.top = "0";
+  } else {
+    document.getElementById("header").style.top = "-200px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
+
+
+
+
+
+//var lastKnownScrollY = 0;
+//var currentScrollY = 0;
+//var ticking = false;
+//var idOfHeader = 'header';
+//var eleHeader = null;
+//const classes = {
+//  pinned: 'header-pin',
+//  unpinned: 'header-unpin',
+//};
+//function onScroll() {
+//  currentScrollY = window.pageYOffset;
+//  requestTick();
+//}
+//function requestTick() {
+//  if (!ticking) {
+//    requestAnimationFrame(update);
+//  }
+//  ticking = true;
+//}
+//function update() {
+//  if (currentScrollY < lastKnownScrollY) {
+//    pin();
+//  } else if (currentScrollY > lastKnownScrollY) {
+//    unpin();
+//  }
+//  lastKnownScrollY = currentScrollY;
+//  ticking = false;
+//}
+//function pin() {
+//    if (eleHeader == null) {
+//        console.log("eleHeader is null, cannot pin")
+//    } else if (eleHeader.classList.contains(classes.unpinned)) {
+//    eleHeader.classList.remove(classes.unpinned);
+//    eleHeader.classList.add(classes.pinned);
+//  }
+//}
+//function unpin() {
+//    if (eleHeader == null) {
+//        console.log("eleHeader is null, cannot unpin")
+//    } else if (eleHeader.classList.contains(classes.pinned) || !eleHeader.classList.contains(classes.unpinned)) {
+//    eleHeader.classList.remove(classes.pinned);
+//    eleHeader.classList.add(classes.unpinned);
+//  }
+//}
+//window.onload = function() {
+//  eleHeader = document.getElementsByClassName("header");
+//     if (eleHeader == null) {
+//        console.log("eleHeader is null, onload() could not initialize it")
+//    }
+//  document.addEventListener('scroll', onScroll, false);
+//}
